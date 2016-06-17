1e68b7f78bd669f684ae967b1bead232ee6a3ade
Biozid02 <biozid0208@gmail.com>
2016-06-17 17:01:18 +0600

    dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,19 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-        @courses = Course.order(:title)
+    @courses = Course.all
   end
 
+  def enroll
+
+    if params[:vaga].nil?
+      redirect_to dashboard_index_path
+
+    else
+    course_object = Course.find(params[:vaga][:id])
+    current_user.courses << ( course_object - current_user.courses )
+
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'home#index'
 
-   resources  :dashboard
-   root 'home#index'
+   resources  :dashboard do
+         collection do
+       post  'enroll'
+     end
+   end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
